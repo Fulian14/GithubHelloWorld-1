683d6d5dcdd1d9976d69f1f0d57882680e83ca3c
amadou <amadou.balde.2023@student.lhschools.org>
2020-02-19 08:57:41 -0500

    called Hello World Method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -2,12 +2,15 @@ public class GithubHelloWorld {
     public static void main(String[] args) {
 System.out.println("Hello World");
        helloWorld();
+       Amadou();
     }
 
     public static void helloWorld(){
         System.out.println("Hello Github World");
     }
-
+    public static void Amadou(){
+        System.out.println("This is Amadou");
+    }
 
 
 
